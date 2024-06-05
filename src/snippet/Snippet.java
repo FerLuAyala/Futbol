1b864823af9539087cbf17cbdc3b9c81d7ada39b
FerLuAyala <fernanda.ayala@davinci.edu.ar>
2024-06-05 17:38:20 -0300

    ult

---
+++ src/snippet/Snippet.java
@@ -0,0 +1,12 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		futbol
+		/futbol/src/futbol/Equipo.java
+		/futbol/src/futbol/Jugador.java
+		/futbol/src/futbol/MainJuego.java
+		/futbol/src/futbol/Torneo.java
+	}
+}
+
